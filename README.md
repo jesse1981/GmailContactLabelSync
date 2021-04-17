137bc27970ae8ba0ae4a0e54ab0b940932afb0e0
Jesse Bryant <jesse.bryant@gmail.com>
2021-04-18 02:42:33 +1000

    First Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# GmailContactLabelSync
+
+Imports your Google contact labels and their respective email addresses, and assigns this label to all emails to which have been sent to or from them.
+
+### Original Credits
+Inspired by the script hosted [here](https://github.com/thirschbuechler/GmailContactGroupLabels)
+
+### How to use
+- goto script.google.com
+- paste the contents of the GmailContactGroupLabels file into the empty Code.gs window
+- replace ContactsGroupName with the contact group of contacts.google.com 
+- replace GmailGroupName with the gmail label of your choice (create the label in gmail first!) 
+- review the code if you want
+- save the file via the menu file+save (in your google drive)
+- click "run" and authenticate the script to access gmail and your contacts
---
+++ index.js
@@ -0,0 +1,81 @@
+/* GmailContactLabelSync G-apps script
+   Jesse Bryant, 2021
+ 
+   Original Author:
+   GmailContactGroupLabels G-apps script
+   t.hirschbuechler, 2017
+   
+   may appear in your gmail account activity (run by a 10.*.*.*	address, corresponding to an internal google-server)
+*/  
+function createNestedGmailLabel(name) {
+  var labels = name.split("/");
+  var gmail, label = "";
+
+  for (var i=0; i<labels.length; i++) {
+
+      if (labels[i] !== "") {
+        label = label + ((i===0) ? "" : "/") + labels[i];
+        gmail = GmailApp.getUserLabelByName(label) ?
+                    GmailApp.getUserLabelByName(label) : GmailApp.createLabel(label);
+    }
+  }
+
+  return gmail;
+
+}
+
+function applyGroupLabels()	{
+  // Variables
+  var threadMax = 20;
+  var threadOffset = 0
+  var parentLabel = "GmailContactLabel"
+
+  // Get all labels and their contacts
+  var contacts = {};
+  var labels = ContactsApp.getContactGroups();
+  for (var l in labels) {
+    label = labels[l].getName();
+    console.log(label);
+    let groupContacts = ContactsApp.getContactGroup(label).getContacts();
+    let emails = [];
+    for (var i in groupContacts) {
+      if (label.indexOf("My Contacts")>-1) continue;
+      try {
+        let EmailField = groupContacts[i].getEmails()
+        for (var j in EmailField){
+          emails.push(EmailField[j].getAddress());
+        }
+        label = label.replace("System Group: ","");
+        contacts[label] = emails;
+        createNestedGmailLabel(parentLabel + "/" + label);
+      }
+      catch (err) {
+        console.log("Caught:",err)
+      }
+      
+    }
+  }
+
+  while (true) {
+    var threadAll = true;
+    for (var label in contacts) {
+      if (contacts[label].length) console.log("Processing:",label)
+      else { console.log("Skipping"); continue; }
+
+      var gmailLabel = GmailApp.getUserLabelByName(parentLabel + "/" + label);
+      var emailString = contacts[label].join(' OR ');
+      var searchString = 'from:(' + emailString + ') OR to:(' + emailString + ')'
+      console.log(searchString)
+
+      var threads = GmailApp.search(searchString,threadOffset,threadMax)
+      for (var x in threads) threads[x].addLabel(gmailLabel);
+
+      console.log("Processed",threads.length,"threads")
+      if (threads.length < threadMax) threadAll = false;
+    }
+    if (threadAll) break;
+    else threadOffset += threadMax;
+  }
+  
+  
+}// applyGroupLabel
